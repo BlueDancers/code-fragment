eca5346e4a0eec859940ee4ea3195d4244d45de8
vkcyan <wu.vkcyan@gmail.com>
2018-09-19 20:35:11 +0800

    ts-1

---
+++ typescript/hello.js
@@ -0,0 +1,5 @@
+function sayHello(person) {
+    return 'hello' + person;
+}
+var user = [1, 2, 3];
+console.log(sayHello(user));
---
+++ typescript/hello.ts
@@ -0,0 +1,5 @@
+function sayHello (person:string) {
+    return 'hello'+person
+}
+let user = [1,2,3]
+console.log(sayHello(user));
