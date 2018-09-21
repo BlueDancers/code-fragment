28b351ab35f010e5eba432cca8cdd7c88020eae2
vkcyan <wu.vkcyan@gmail.com>
2018-09-21 18:26:28 +0800

    ts打卡

--- typescript/hello.js
+++ typescript/hello.js
@@ -1,5 +1,18 @@
 function sayHello(person) {
     return 'hello' + person;
 }
-var user = [1, 2, 3];
+var user = 'vkcyan';
 console.log(sayHello(user));
+var isDone = false;
+var Bool = Boolean(1);
+var de = 8;
+var no = NaN;
+var infin = Infinity;
+var Name = '111';
+var Age = '25';
+function alertName() {
+    console.log('name');
+}
+var NNN = 'seven';
+NNN = 7;
+console.log(NNN.fileName);
--- typescript/hello.ts
+++ typescript/hello.ts
@@ -1,5 +1,26 @@
-function sayHello (person:string) {
-    return 'hello'+person
+function sayHello(person: string) {
+    return 'hello' + person
 }
-let user = [1,2,3]
+let user = 'vkcyan'
 console.log(sayHello(user));
+
+let isDone: boolean = false
+
+let Bool: boolean = Boolean(1)
+let de:number = 8
+let no:number = NaN
+let infin:number = Infinity
+let Name: string = '111'
+let Age : string = '25'
+
+
+function alertName() : void{
+    console.log('name')
+}
+let NNN : any = 'seven'
+
+NNN = 7
+
+console.log(NNN.fileName);
+
+
